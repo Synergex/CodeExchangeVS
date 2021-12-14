d00ea2c6f8e31464b533ec211fb11394d422067c
denisesmelich <56693379+denisesmelich@users.noreply.github.com>
2021-12-14 07:39:18 -0800

    Add initial files for CodeExchange

---
+++ CodeExchange/clean.bat
@@ -0,0 +1,11 @@
+cd C:\Development\CodeExchange\
+del/s/q *.dbh
+del/s/q *.dbp
+del/s/q *.dbo
+del/s/q *.dbr
+del/s/q *.olb
+del/s/q *.elb
+del/s/q *.vtg
+del/s/q *.vpwhist
+del/s build*.bat
+dir/AD/S _sgbak
